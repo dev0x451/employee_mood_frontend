98d3d21c19b4648c76f352a1901689acc1d657da
Anastasia Sergeeva <lirebook@yandex.ru>
2023-05-22 22:45:03 +0300

    feat: add ProtectedRoutes Component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import {
   Route,
   Routes,
@@ -8,18 +8,22 @@ import {Navbar} from '../Navbar/Navbar';
 import {Guestpage} from '../Guestpage/Guestpage';
 
 import styles from './app.module.css';
+import {ProtectedRoutes} from "@/components/ProtectedRoutes";
+import LoginPage from "@/components/LoginPage/LoginPage";
 
 export const App = () => {
+
+  const [loggedIn] = useState(false);
 
   return (
     <main className={styles.page}>
       <Navbar/>
       <Routes>
-
-        <Route path='home' element={<Homepage/>}/>
-
-        <Route path='guest' element={<Guestpage/>}/>
-
+        <Route element={<ProtectedRoutes loggedIn={loggedIn} />}>
+          <Route path='home' element={<Homepage/>}/>
+          <Route path='guest' element={<Guestpage/>}/>
+        </Route>
+        <Route path='sign-in' element={<LoginPage/>}/>
       </Routes>
     </main>
   )
