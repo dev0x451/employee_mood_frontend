41e0e018f551684ca8caf98726efff86a1b18cab
Alix2019 <alix1982@yandex.ru>
2023-05-23 21:17:05 +0300

    add_component_Event

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -1,10 +1,26 @@
 import styles from './events.module.css';
-
+import image from '../../assets/image.png'
 export const Events = () => {
 
   return (
     <div className={styles.events}>
-      <p className={styles.text}>Events</p>
+      <h3 className={styles.header}>Предстоящее мероприятие</h3>
+      <ul className={styles.list}>
+        <li className={styles.point}>
+          <img  className={styles.img} src={image}/>
+          <div  className={styles.pointContent}>
+            <p className={styles.text}>Тимбилдинг</p>
+            <time className={styles.time}>18 мая 2023</time>
+          </div>
+        </li>
+        <li className={styles.point}>
+          <img  className={styles.img} src={image}/>
+          <div  className={styles.pointContent}>
+            <p className={styles.text}>Тимбилдинг</p>
+            <time className={styles.time}>18 мая 2023</time>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
