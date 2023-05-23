26dc0ab109f1a5c05bedb07649723d11462acfef
Anastasia Sergeeva <lirebook@yandex.ru>
2023-05-23 03:20:59 +0300

    fix: delete NavBar from App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "../Homepage/Homepage";
-import { Navbar } from "../Navbar/Navbar";
 import { Guestpage } from "../Guestpage/Guestpage";
 
 import styles from "./app.module.css";
@@ -13,7 +12,6 @@ export const App = () => {
 
   return (
     <main className={styles.page}>
-      <Navbar />
       <Routes>
         <Route element={<ProtectedRoutes loggedIn={loggedIn} />}>
           <Route path="home" element={<Homepage />} />
