78a38eda02f5427f181b5699abbd456b10998187
Anastasia Sergeeva <lirebook@yandex.ru>
2023-06-02 10:07:56 +0300

    fix: delete icon search and make it background

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import styles from "./header.module.css";
-import icon from "@/assets/search_icon.svg";
 import { NavLink } from "react-router-dom";
 import { LogoSideMenu } from "@/shared/ui/LogoSideMenu/LogoImg";
 
@@ -14,7 +13,6 @@ export const Header = () => {
         name="main-search-input"
         placeholder="Поиск"
       />
-      <img className={styles.searchIcon} src={icon} alt="search icon" />
       <div className={styles.profileAndNotify}>
         <div className={styles.notify}></div>
         <div className={styles.profile}></div>
