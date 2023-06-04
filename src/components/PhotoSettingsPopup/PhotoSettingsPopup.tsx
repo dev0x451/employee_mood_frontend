3375b5495a99f0500bd92c98ec4675cfd3c12b50
Anastasia Sergeeva <lirebook@yandex.ru>
2023-06-04 22:47:28 +0300

    feat: finish layout of account page, connect with backend

--- src/components/PhotoSettingsPopup/PhotoSettingsPopup.tsx
+++ src/components/PhotoSettingsPopup/PhotoSettingsPopup.tsx
@@ -5,8 +5,14 @@ import DeleteIcon from "./door_20.svg";
 
 interface Props {
   isPhotoClicked: boolean;
+  uploadPhoto: (e: any) => void;
+  removePhoto: () => void;
 }
-export const PhotoSettingsPopup: React.FC<Props> = ({ isPhotoClicked }) => {
+export const PhotoSettingsPopup: React.FC<Props> = ({
+  isPhotoClicked,
+  uploadPhoto,
+  removePhoto,
+}) => {
   return (
     <ul
       className={
@@ -21,9 +27,21 @@ export const PhotoSettingsPopup: React.FC<Props> = ({ isPhotoClicked }) => {
           alt="иконка загрузки фотографии"
           className={styles.photoSettingsIcon}
         />
-        <p className={styles.photoSettingsText}>Загрузить фотографию</p>
+        <label className={styles.photoSettingsText} htmlFor="photo">
+          Загрузить фотографию
+        </label>
+        <input
+          onChange={(e) => uploadPhoto(e)}
+          type="file"
+          id="photo"
+          accept=".jpg, .jpeg, .png"
+          className={styles.hidden}
+        />
       </li>
-      <li className={styles.photoSettingsListItem}>
+      <li
+        onClick={() => removePhoto()}
+        className={`${styles.photoSettingsListItem} ${styles.photoSettingsListItemDelete}`}
+      >
         <img
           src={DeleteIcon}
           alt="иконка удаления фотографии"
--- src/store/reducers/currentUser/currentUserReducer.ts
+++ src/store/reducers/currentUser/currentUserReducer.ts
@@ -1,40 +1,90 @@
-import { createSlice } from '@reduxjs/toolkit';
-import type { RootState } from '../../store';
-import { User } from '@/types';
+import { createSlice } from "@reduxjs/toolkit";
+import type { RootState } from "../../store";
+import { User } from "@/types";
 
 const initialState: User = {
-  about: '',
-  avatar: '',
-  date_joined: '',
+  about: "",
+  avatar: "",
+  date_joined: "",
   department: null,
-  email: '',
-  first_name: '',
+  email: "",
+  first_name: "",
   hobbies: null,
   id: 0,
-  last_name: '',
+  last_name: "",
   latest_condition: null,
-  mental_state: '',
-  patronymic: '',
+  mental_state: "",
+  patronymic: "",
   phone: 0,
   position: null,
-  role: '',
+  role: "",
 };
 
 export const currentUserSlice = createSlice({
-  name: 'constructorSlice',
+  name: "constructorSlice",
   initialState,
-  reducers:
-  {
+  reducers: {
     setCurrentUser: (state, action) => {
       state.role = action.payload;
     },
     resetCurrentUser: (state) => {
-      state.role = '';
-    }
+      state.role = "";
+    },
+    setCurrentUserFirstName: (state, action) => {
+      state.first_name = action.payload;
+    },
+    resetCurrentUserFirstName: (state) => {
+      state.first_name = "";
+    },
+    setCurrentUserLastName: (state, action) => {
+      state.last_name = action.payload;
+    },
+    resetCurrentUserLastName: (state) => {
+      state.last_name = "";
+    },
+    setCurrentUserPosition: (state, action) => {
+      state.position = action.payload;
+    },
+    resetCurrentUserPosition: (state) => {
+      state.position = "";
+    },
+    setCurrentUserAbout: (state, action) => {
+      state.about = action.payload;
+    },
+    resetCurrentUserAbout: (state) => {
+      state.about = "";
+    },
+    setCurrentUserAvatar: (state, action) => {
+      state.avatar = action.payload;
+    },
+    resetCurrentUserAvatar: (state) => {
+      state.avatar = "";
+    },
   },
 });
 
 export const selectRole = (state: RootState) => state.currentUserSlice.role;
-export const { setCurrentUser, resetCurrentUser } = currentUserSlice.actions;
+export const selectFirstName = (state: RootState) =>
+  state.currentUserSlice.first_name;
+export const selectLastName = (state: RootState) =>
+  state.currentUserSlice.last_name;
+export const selectPosition = (state: RootState) =>
+  state.currentUserSlice.position;
+export const selectAbout = (state: RootState) => state.currentUserSlice.about;
+export const selectAvatar = (state: RootState) => state.currentUserSlice.avatar;
+export const {
+  setCurrentUser,
+  resetCurrentUser,
+  setCurrentUserFirstName,
+  setCurrentUserLastName,
+  setCurrentUserPosition,
+  setCurrentUserAbout,
+  setCurrentUserAvatar,
+  resetCurrentUserFirstName,
+  resetCurrentUserLastName,
+  resetCurrentUserPosition,
+  resetCurrentUserAbout,
+  resetCurrentUserAvatar,
+} = currentUserSlice.actions;
 
 export default currentUserSlice.reducer;
