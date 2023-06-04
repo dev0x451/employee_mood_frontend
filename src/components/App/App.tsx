98c110022c5463444d876ba93881ee0ad3f3188a
RomanRiyanov <yezgo123@gmail.com>
2023-06-04 17:45:14 +0600

    fix/tests fix signout bug

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -16,7 +16,7 @@ import { ProtectedRoutes } from "@/components/ProtectedRoutes";
 import { RegisterPage } from "@/pages/register/RegisterPage";
 import { RefreshPasswordPage } from "@/pages/refreshpassword/RefreshPasswordPage";
 import { LoginPage } from "@/pages/login/LoginPage";
-import { MyFormValues, TestResult, ExpressDiagnoseResponse } from "@/types";
+import { MyFormValues, TestResult, ExpressDiagnoseResponse, TestInterface } from "@/types";
 import * as ApiAuth from "@/shared/api/ApiAuth";
 import * as Api from "@/shared/api/Api";
 import { useLocation } from "react-router";
@@ -31,6 +31,7 @@ export const App = () => {
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
   const [resultOfPsychoTest, setResultOfPsychoTest] = useState<ExpressDiagnoseResponse>();
+  const [expressTest, setExpressTest] = useState<TestInterface| null>(null)
   const [allTestsResults, setallTestsResults] = useState<ExpressDiagnoseResponse[]>()
   const [isLoading, setIsLoading] = useState(false);
   const [employees, setEmployees] = useState([]);
@@ -157,6 +158,11 @@ export const App = () => {
     }
   }
 
+
+
+
+
+
   async function handleSendTestResult(result: TestResult) {
     try {
       const response = await Api.sendTestResults(result);
@@ -168,13 +174,41 @@ export const App = () => {
   }
 
   async function getAllTestsResult() {
-    try {
+      try {
       const response = await Api.getAllTestsResults();
       setallTestsResults(response.data.results);
     } catch (err: any) {
       console.log(err);
     }
   }
+
+  async function getTestsQuestions() {
+      try {
+      const response = await Api.getTestQuestions('1');
+      setExpressTest(response.data);
+    } catch (err: any) {
+      console.log(err);
+    }
+  }
+
+  // if (loggedIn) {
+    // const [expressTest] = useRequest(() => Api.getTestQuestions("1"));
+  // }
+
+
+  useEffect(() => {
+    if (loggedIn) {
+      getAllTestsResult();
+      getTestsQuestions();
+    }
+  }, [loggedIn])
+
+
+
+
+
+
+
 
   async function handleEmployees() {
     try {
@@ -192,7 +226,6 @@ export const App = () => {
   // useEffect(()=>{handleEmployees()},[loggedIn]);
   useEffect(()=>{handleEmployees()},[role]);
 
-  const [expressTest] = useRequest(() => Api.getTestQuestions("1"));
 
   const closeErrorPopup = () => {
     setPopupOpened(false);
@@ -203,10 +236,6 @@ export const App = () => {
     setError("");
     setSuccess("");
   };
-
-  useEffect(() => {
-    getAllTestsResult()
-  }, [])
 
   if (isLoading) {
     return <div></div>;
--- src/components/Records/Records.tsx
+++ src/components/Records/Records.tsx
@@ -1,5 +1,5 @@
 import styles from "./recodrds.module.css";
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { sortIcon } from "@/assets";
 import { TestResultPopup } from "../TestResultPopup/TestResultPopup";
 import { ExpressDiagnoseResponse } from "@/types";
@@ -8,9 +8,74 @@ interface Records {
   allTestsResults?: ExpressDiagnoseResponse[]
 }
 
+//необходим полный рефакторинг логики сортировки
+
 export const Records: React.FC<Records> = ({allTestsResults}) => {
   const [isOpen, setOpen] = useState<boolean>(false);
   const [result, setResult] = useState<ExpressDiagnoseResponse>();
+
+
+  const [testResultsSort, setTestResultsSort] = useState(allTestsResults)
+  const [isSortName, setIsSortName] = useState(true)
+  const [isSortPosition, setIsSortPosition] = useState(true)
+  const [isSortState, setIsSortState] = useState(true)
+
+  useEffect(()=>{
+    if (allTestsResults) setTestResultsSort(allTestsResults)
+  },[allTestsResults])
+
+//необходим полный рефакторинг логики сортировки
+
+  const sortField =
+    (
+      a:{result: string, completion_date: string},
+      b:{result: string, completion_date: string},
+      field: string
+    ) => {
+    let x = '';
+    let y = '';
+    switch(field) {
+      case 'name':
+        x = a.result + a.result;
+        y = b.result + b.result;
+      break;
+      case 'position':
+        x = a.completion_date;
+        y = b.completion_date;
+      break;
+      // case 'state':
+      //   x = a.mental_state;
+      //   y = b.mental_state;
+      // break;
+      default:
+        x = '';
+        y = '';
+      break;
+    }
+    if (x < y) {return -1}
+    if (x > y) {return 1}
+    return 0;
+  }
+
+  const sortFields = (field: string, isSortField: boolean) => {
+    if (testResultsSort)
+    isSortField ?
+    setTestResultsSort(testResultsSort.sort((a, b)=>sortField(a, b, field))) :
+    setTestResultsSort(testResultsSort.sort((b, a)=>sortField(a, b, field)))
+  }
+
+  const sortName = () => {
+    setIsSortName(!isSortName);
+    sortFields('name', isSortName);
+  }
+  const sortPosition = () => {
+    setIsSortPosition(!isSortPosition);
+    sortFields('position', isSortPosition);
+  }
+  const sortState = () => {
+    setIsSortState(!isSortState);
+    sortFields('state', isSortState);
+  }
 
   function handleOpenPopup (record: ExpressDiagnoseResponse) {
     setOpen(true);
@@ -24,9 +89,9 @@ export const Records: React.FC<Records> = ({allTestsResults}) => {
   return (
     <div className={styles.recodrds}>
       <section className={styles.tableRecords}>
-        <button className={styles.sortButton}>Наименование опроса {sortIcon}</button>
-        <button className={styles.sortButton}>Дата проведения {sortIcon}</button>
-        <button className={styles.sortButton}>Результат {sortIcon}</button>
+        <button className={styles.sortButton} onClick={sortName}>Наименование опроса {sortIcon}</button>
+        <button className={styles.sortButton} onClick={sortName}>Дата проведения {sortIcon}</button>
+        <button className={styles.sortButton} onClick={sortName}>Результат {sortIcon}</button>
       </section>
       {allTestsResults && allTestsResults.map((record, index) => (
       <section key={index} className={styles.record}>
