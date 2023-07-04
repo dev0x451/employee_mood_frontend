2573820543668044c7ad294e00877015dda4481c
Anastasia Sergeeva <lirebook@yandex.ru>
2023-07-04 16:56:20 +0300

    fix: fixes

--- src/pages/balancewheel/components/BalanceWheelResult/BalanceWheelResult.tsx
+++ src/pages/balancewheel/components/BalanceWheelResult/BalanceWheelResult.tsx
@@ -77,14 +77,13 @@ export const BalanceWheelResult = ({ step, goToFirstQuestion, data }: BalanceWhe
     }
   }, [priorityResults, currentResults]);
 
-
   return (
     <div className={pathname !== "/balance-wheel" ? styles.container : ""}>
       <div className={styles.resultArea}>
         {pathname === "/balance-wheel"
           ?
           <div className={styles.resultInfo}>
-            <h4 className={styles.title}>Ваше колесо жизненного баланса готово</h4>
+            <h4 className={styles.title}>Ваше колесо жизненного баланса готово!</h4>
             <ul className={styles.legend}>
               <li className={styles.legendItem}>
                 <div className={`${styles.legendColor} ${styles.legendColorPriority}`}></div>
